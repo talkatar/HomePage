ebb1d0881b2d138fbac80bc74085cf28f5a924bb
Tal Katar <talkatar120@gmail.com>
2023-04-14 20:51:19 +0300

    push

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/HomePage/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "HomePage" : "/",
   // build:{
   //   outDir:'../backend/public',
   //   emptyOutDir:true
